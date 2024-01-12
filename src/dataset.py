56454b18af442a09d8ec00bcceaff6b42520b45a
Anatol Garioud <49435550+agarioud@users.noreply.github.com>
2024-01-12 14:18:48 +0100

    Add files via upload

---
+++ src/dataset.py
@@ -0,0 +1,142 @@
+import numpy as np
+import logging
+import rasterio
+from skimage import img_as_float
+
+import torch
+from torch.utils.data import Dataset
+
+
+def norm(in_img, norm_type=None, means=[], stds=[]):
+    in_img = in_img.astype(np.float64) 
+    if norm_type not in ['scaling','custom','without']:
+            logging.error("Normalization argument should be 'scaling', 'custom' or 'without'.")
+            raise SystemExit()
+    if norm_type: 
+        if norm_type == 'custom':
+            if len(means) != len(stds):
+                logging.error("If custom, provided normalization means and stds should be of same lenght.")
+                raise SystemExit()                
+            else:
+                for i in range(len(means)):
+                    in_img[i] -= means[i]
+                    in_img[i] /= stds[i]
+        elif norm_type == 'scaling':
+            in_img = img_as_float(in_img)
+    return in_img
+
+
+
+class fit_dataset(Dataset):
+
+    def __init__(self,
+                 dict_files,
+                 channels=[1,2,3,4,5],
+                 num_classes=13, 
+                 use_metadata=True,
+                 use_augmentations=None,
+                 norm_type='scale',
+                 means=[],
+                 stds=[]
+                 ):
+
+        self.list_imgs = np.array(dict_files["IMG"])
+        self.list_msks = np.array(dict_files["MSK"])
+        self.use_metadata = use_metadata
+        if use_metadata == True:
+            self.list_metadata = np.array(dict_files["MTD"])
+        self.use_augmentations = use_augmentations
+        self.channels = channels
+        self.num_classes = num_classes
+        self.norm_type= norm_type
+        self.means = means
+        self.stds = stds
+
+
+    def read_img(self, raster_file: str) -> np.ndarray:
+        with rasterio.open(raster_file) as src_img:
+            array = src_img.read(self.channels)
+            return array
+
+    def read_msk(self, raster_file: str) -> np.ndarray:
+        with rasterio.open(raster_file) as src_msk:
+            array = src_msk.read()[0]
+            array[array > self.num_classes] = self.num_classes
+            array = array-1
+            array = np.stack([array == i for i in range(self.num_classes)], axis=0)
+            return array  
+
+    def __len__(self):
+        return len(self.list_imgs)
+
+    def __getitem__(self, index):
+        image_file = self.list_imgs[index]
+        img = self.read_img(raster_file=image_file)
+        
+        mask_file = self.list_msks[index]
+        msk = self.read_msk(raster_file=mask_file)
+
+        if self.use_augmentations is not None:
+            sample = {"image" : img.swapaxes(0, 2).swapaxes(0, 1), "mask": msk.swapaxes(0, 2).swapaxes(0, 1)}
+            transformed_sample = self.use_augmentations(**sample)
+            img, msk = transformed_sample["image"].swapaxes(0, 2).swapaxes(1, 2).copy(), transformed_sample["mask"].swapaxes(0, 2).swapaxes(1, 2).copy()            
+        
+        img = norm(img, norm_type=self.norm_type, means=self.means, stds=self.stds)
+
+        if self.use_metadata == True:
+            mtd = self.list_metadata[index]
+            return {"img": torch.as_tensor(img, dtype=torch.float), 
+                    "mtd": torch.as_tensor(mtd, dtype=torch.float),
+                    "msk": torch.as_tensor(msk, dtype=torch.float)}
+        else:
+            return {"img": torch.as_tensor(img, dtype=torch.float), 
+                    "msk": torch.as_tensor(msk, dtype=torch.float)}  
+
+
+
+
+class predict_dataset(Dataset):
+
+    def __init__(self,
+                 dict_files,
+                 channels=[1,2,3,4,5],
+                 num_classes=13, 
+                 use_metadata=True,
+                 norm_type='scaling',
+                 means=[],
+                 stds=[]
+                 ):
+        
+        self.list_imgs = np.array(dict_files["IMG"])
+        self.num_classes = num_classes
+        self.use_metadata = use_metadata
+        if use_metadata == True:
+            self.list_metadata = np.array(dict_files["MTD"])
+        self.channels = channels
+        self.norm_type= norm_type
+        self.means = means
+        self.stds = stds
+
+    def read_img(self, raster_file: str) -> np.ndarray:
+        with rasterio.open(raster_file) as src_img:
+            array = src_img.read(self.channels)
+        return array
+        
+    def __len__(self):
+        return len(self.list_imgs)
+
+    def __getitem__(self, index):
+        image_file = self.list_imgs[index]
+        img = self.read_img(raster_file=image_file)
+        
+        img = norm(img, norm_type=self.norm_type, means=self.means, stds=self.stds)
+
+        if self.use_metadata == True:
+            mtd = self.list_metadata[index]
+            return {"img": torch.as_tensor(img, dtype=torch.float), 
+                    "mtd": torch.as_tensor(mtd, dtype=torch.float),
+                    "id": image_file}
+        else:
+           
+            return {"img": torch.as_tensor(img, dtype=torch.float),
+                    "id": image_file}  
---
+++ src/model_utils.py
@@ -0,0 +1,113 @@
+import torch
+from torch import nn
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import albumentations as A
+
+from .model import smp_unet_mtd
+from .data_module import flair_datamodule
+from .task_module import segmentation_task_training, segmentation_task_predict
+
+
+
+def get_data_module(config, dict_train=None, dict_val=None, dict_test=None):
+    """
+    This function creates a data module for training, validation, and testing.
+    
+    Parameters:
+    config (dict): Configuration dictionary containing parameters for the data module.
+    dict_train (dict): Dictionary containing training data.
+    dict_val (dict): Dictionary containing validation data.
+    dict_test (dict): Dictionary containing test data.
+
+    Returns:
+    dm: Data module with specified configuration.
+    """
+    assert isinstance(config, dict), "config must be a dictionary"
+    assert isinstance(config["use_augmentation"], bool), "use_augmentation must be a boolean"
+    assert isinstance(config["use_metadata"], bool), "use_metadata must be a boolean"   
+    
+    if config["use_augmentation"]:
+        transform_set = A.Compose([A.VerticalFlip(p=0.5),
+                                   A.HorizontalFlip(p=0.5),
+                                   A.RandomRotate90(p=0.5)]
+        )
+    else:
+        transform_set = None
+
+    dm = flair_datamodule(
+        dict_train = dict_train,
+        dict_val = dict_val,
+        dict_test = dict_test,
+        batch_size = config["batch_size"],
+        num_workers = config["num_workers"],
+        drop_last = True,
+        num_classes = config["num_classes"],
+        channels=config["channels"],
+        use_metadata = config["use_metadata"],
+        use_augmentations = transform_set,
+        norm_type = config["norm_type"],
+        means = config["norm_means"],
+        stds = config["norm_stds"]
+    )
+    
+    return dm
+
+
+
+def get_segmentation_module(config, stage='train'):
+    """
+    This function creates a segmentation module for training or prediction.
+    
+    Parameters:
+    config (dict): Configuration dictionary containing parameters for the segmentation module.
+    stage (str): Stage for which the segmentation module is created ('train' or 'predict').
+
+    Returns:
+    seg_module: Segmentation module with specified configuration.
+    """
+    assert stage in ['train', 'predict'], "stage must be either 'train' or 'predict'"
+    
+    # Define model
+    model = smp_unet_mtd(architecture = config['model_architecture'],
+                         encoder = config['encoder_name'],
+                         n_channels = len(config["channels"]), 
+                         n_classes = config["num_classes"], 
+                         use_metadata = config["use_metadata"]
+    )
+    
+    if stage == 'train':
+        if config["use_weights"]:
+            with torch.no_grad():
+                class_weights = torch.FloatTensor(config["class_weights"])
+            criterion = nn.CrossEntropyLoss(weight=class_weights)
+        else:
+            criterion = nn.CrossEntropyLoss()
+
+        optimizer = torch.optim.SGD(model.parameters(), lr=config["learning_rate"])
+
+        scheduler = ReduceLROnPlateau(
+            optimizer = optimizer,
+            mode = "min",
+            factor = 0.5,
+            patience = 10,
+            cooldown = 4,
+            min_lr = 1e-7,
+        )
+
+        seg_module = segmentation_task_training(
+            model = model,
+            num_classes = config["num_classes"],
+            criterion = criterion,
+            optimizer = optimizer,
+            scheduler = scheduler,
+            use_metadata=config["use_metadata"]
+        )
+
+    elif stage == 'predict':
+        seg_module = segmentation_task_predict(
+            model = model,
+            num_classes = config["num_classes"],
+            use_metadata=config["use_metadata"],
+        )        
+
+    return seg_module
