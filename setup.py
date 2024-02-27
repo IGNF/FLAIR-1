9cb47413497213d1474effb98f768f941230842d
Anatol Garioud <49435550+agarioud@users.noreply.github.com>
2024-02-27 23:37:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-import os, sys
-
-from pathlib import Path
 from setuptools import setup, find_packages
 
 setup(
@@ -8,12 +5,12 @@ setup(
     version='0.1.0',  # Change this as needed or implement dynamic version reading from VERSION file
     author='Anatol Garioud & Samy Khelifi',
     author_email='ai-challenge@ign.fr',
-    description='baseline and demo code for flair 1 challenge',
+    description='baseline and demo code from the flair #1 challenge',
     long_description='French Land-cover from Arospace ImageRy',
     long_description_content_type='French Land-cover from Arospace ImageRy',
-    url='https://github.com/IGNF/FLAIR-1-AI-Challenge',
+    url='https://github.com/IGNF/FLAIR-1',
     project_urls={
-        'Bug Tracker': 'https://github.com/IGNF/FLAIR-1-AI-Challenge'
+        'Bug Tracker': 'https://github.com/IGNF/FLAIR-1'
     },
     classifiers=[
         'Programming Language :: Python :: 3',
@@ -31,7 +28,17 @@ setup(
         'geopandas>=0.10',
         'rasterio>=1.1.5',
         'omegaconf',
-        'jsonargparse'
+        'jsonargparse',
+        "matplotlib>=3.8.2",
+        "pandas>=2.1.4",
+        "scikit-image>=0.22.0",
+        "pillow>=9.3.0",
+        "torchmetrics==1.2.0",
+        "pytorch-lightning==2.1.1",
+        "segmentation-models-pytorch==0.3.3",
+        "albumentations==1.3.1",
+        "rasterio==1.3.6",
+        "tensorboard==2.15.1"
     ],
     include_package_data=True,
     package_data={
@@ -39,8 +46,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'flair-detect=src.detect.main:main',
-            'flair-train=main:main'
+            'flair-detect=src.zone_detect.main:main',
+            'flair-train=src.flair_patch.main:main'
         ]
     }
 )
