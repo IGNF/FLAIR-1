5f106412d8264d745e5737d0edc3d8e3fd5078d8
Anatol Garioud <49435550+agarioud@users.noreply.github.com>
2024-02-27 23:35:38 +0100

    Delete src directory

--- src/detect/types.py
+++
@@ -1,6 +0,0 @@
-from typing import TypeAlias, Literal, List, Dict, Tuple, Any
-
-GEO_FLOAT_TUPLE: TypeAlias = Tuple[float, float]
-GEO_INT_TUPLE: TypeAlias = Tuple[int, int]
-OUTPUT_TYPE: TypeAlias = Literal['uint8', 'float32', 'bit', 'argmax']
-PARAMS: TypeAlias = Dict[str, Any]
