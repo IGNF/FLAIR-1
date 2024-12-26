b60a0a2ee56c7bb6b8dc118687f4a9a7d9a0791b
Matthieu CHAMBERY <matthieu.chambery@netcarbon.fr>
2024-12-26 17:44:52 +0100

    feature(flair_detect_service): separate exceptions handling from service logic + do force garbage collection at the end

--- src/api/flair_detect_service.py
+++ src/api/flair_detect_service.py
@@ -1,9 +1,10 @@
 import os
+from time import perf_counter
 from typing import Tuple
-from subprocess import CalledProcessError, run
+from subprocess import run
+from uuid import uuid4
 
-from fastapi import HTTPException
-from google.cloud.exceptions import NotFound, Forbidden
+import torch
 from google.cloud.storage import Client
 
 from src.api.handle_files import (
@@ -17,7 +18,14 @@ from src.api.classes.prediction_models import (
     Rgbi15clResnet34UnetModel,
     FlairModel,
 )
-from src.constants import DATA_FOLDER, INPUT_FOLDER
+from src.api.setup_flair_configs import setup_config_flair_detect
+from src.constants import (
+    DATA_FOLDER,
+    INPUT_FOLDER,
+    OUTPUT_FOLDER,
+    FLAIR_GCP_PROJECT,
+    FLAIR_DETECT_BATCH_SIZE,
+)
 
 logger = get_logger()
 
@@ -57,32 +65,13 @@ def download_file_to_process(
 ):
     image_name = os.path.basename(image_blob_path)
     image_local_path = os.path.join(input_folder, image_name)
-    try:
-        download_file(
-            bucket_name=image_bucket_name,
-            blob_path=image_blob_path,
-            local_path=image_local_path,
-            client=client,
-        )
-    except NotFound as e:
-        raise HTTPException(
-            status_code=404,
-            detail=f"Blob {image_blob_path} in bucket {image_bucket_name} \
-                    not found : {str(e)}",
-        ) from e
-    except Forbidden as e:
-        raise HTTPException(
-            status_code=403,
-            detail=f"Permission error during download of blob \
-                    {image_blob_path} in bucket {image_bucket_name} \
-                    : {str(e)}",
-        ) from e
-    except Exception as e:
-        raise HTTPException(
-            status_code=500,
-            detail=f"Unexpected exception during download of blob \
-                {image_blob_path} in bucket {image_bucket_name} : {str(e)}",
-        ) from e
+
+    download_file(
+        bucket_name=image_bucket_name,
+        blob_path=image_blob_path,
+        local_path=image_local_path,
+        client=client,
+    )
 
     logger.info(
         "Blob %s in bucket %s downloaded at %s",
@@ -95,24 +84,12 @@ def download_file_to_process(
 
 
 def run_prediction(prediction_config_path: str):
-    try:
-        result = run(
-            ["flair-detect", "--conf", prediction_config_path],
-            check=True,
-            capture_output=True,
-            text=True,
-        )
-    except CalledProcessError as e:
-        raise HTTPException(
-            status_code=500,
-            detail=f"Error executing flair-detect script: {e.stderr.strip()}",
-        ) from e
-    except Exception as e:
-        raise HTTPException(
-            status_code=500,
-            detail=f"Unexpected exception during flair-detect script : \
-                {str(e)}",
-        ) from e
+    result = run(
+        ["flair-detect", "--conf", prediction_config_path],
+        check=True,
+        capture_output=True,
+        text=True,
+    )
 
     logger.info(
         "Prediction with config %s done with success", prediction_config_path
@@ -129,28 +106,93 @@ def upload_result_to_bucket(
     client: Client,
 ):
     output_path = os.path.join(output_prediction_folder, output_name)
-    try:
-        upload_file(
-            bucket_name=output_bucket_name,
-            blob_path=output_blob_path,
-            local_path=output_path,
-            client=client,
-        )
-    except NotFound as e:
-        raise HTTPException(
-            status_code=404,
-            detail=f"Bucket {output_bucket_name} for upload not found : \
-                {str(e)}",
-        ) from e
-    except Forbidden as e:
-        raise HTTPException(
-            status_code=403,
-            detail=f"Permission error during upload of blob \
-                {output_blob_path} in bucket {output_bucket_name} : {str(e)}",
-        ) from e
-    except Exception as e:
-        raise HTTPException(
-            status_code=500,
-            detail=f"Unexpected exception during upload of blob \
-                {output_blob_path} in bucket {output_bucket_name} : {str(e)}",
-        ) from e
+    upload_file(
+        bucket_name=output_bucket_name,
+        blob_path=output_blob_path,
+        local_path=output_path,
+        client=client,
+    )
+
+
+def flair_detect_service(
+    image_bucket_name: str,
+    image_blob_path: str,
+    model: SupportedModel,
+    output_bucket_name: str,
+    output_blob_path: str,
+):
+    # Set identifier for the current prediction (avoid overlap with async calls
+    prediction_id = str(uuid4())
+
+    # Create output folder for the prediction
+    output_prediction_folder = os.path.join(OUTPUT_FOLDER, prediction_id)
+    os.makedirs(output_prediction_folder, exist_ok=True)
+
+    # Google clood storage client
+    client = Client(project=FLAIR_GCP_PROJECT)
+
+    # Download requested model from netcarbon gcs
+    flair_model, model_weights_path = get_requested_model(
+        model=model, client=client
+    )
+
+    # Download file to process
+    start_time = perf_counter()
+    image_local_path = download_file_to_process(
+        image_bucket_name=image_bucket_name,
+        image_blob_path=image_blob_path,
+        client=client,
+    )
+    download_file_duration = int(round(perf_counter() - start_time))
+
+    logger.info("download file duration: %s seconds", download_file_duration)
+
+    # Setup flair-detect config
+    output_name = os.path.basename(output_blob_path)
+    prediction_config_path = setup_config_flair_detect(
+        input_image_path=image_local_path,
+        model_weights_path=model_weights_path,
+        output_image_name=output_name,
+        output_folder=output_prediction_folder,
+        batch_size=FLAIR_DETECT_BATCH_SIZE,
+    )
+    logger.info(
+        "Config setup for flair-detect for model %s and image %s",
+        flair_model.name,
+        image_blob_path,
+    )
+
+    # Run the prediction with flair-detect script
+    use_gpu = torch.cuda.is_available()
+    logger.info("cuda is available : %s", use_gpu)
+
+    start_time = perf_counter()
+    result = run_prediction(prediction_config_path=prediction_config_path)
+    run_prediction_duration = int(round(perf_counter() - start_time))
+
+    logger.info("run prediction duration: %s seconds", run_prediction_duration)
+
+    # Upload resulted tif to bucket
+    start_time = perf_counter()
+    upload_result_to_bucket(
+        output_prediction_folder=output_prediction_folder,
+        output_name=output_name,
+        output_bucket_name=output_bucket_name,
+        output_blob_path=output_blob_path,
+        client=client,
+    )
+    upload_result_duration = int(round(perf_counter() - start_time))
+
+    logger.info("upload result duration: %s seconds", upload_result_duration)
+
+    return {
+        "prediction_id": prediction_id,
+        "message": f"prediction tif is available at gs://{output_bucket_name}/{output_blob_path}",
+        "cuda_used": use_gpu,
+        "result_stdout": result.stdout,
+        "perf": {
+            "download_file_duration": download_file_duration,
+            "run_prediction_duration": run_prediction_duration,
+            "upload_result_duration": upload_result_duration,
+        },
+    }
--- src/api/main.py
+++ src/api/main.py
@@ -1,28 +1,17 @@
-import os
-from time import perf_counter
+import gc
+from subprocess import CalledProcessError
 from typing import Annotated
-from uuid import uuid4
 
-import torch
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import HTTPAuthorizationCredentials
-from google.cloud.storage import Client
+from google.cloud.exceptions import NotFound, Forbidden
 
 from src.api.classes.prediction_models import SupportedModel
 from src.api.flair_detect_service import (
-    get_requested_model,
-    download_file_to_process,
-    run_prediction,
-    upload_result_to_bucket,
+    flair_detect_service,
 )
 from src.api.security import verify_token
 from src.api.logger import get_logger
-from src.api.setup_flair_configs import setup_config_flair_detect
-from src.constants import (
-    FLAIR_GCP_PROJECT,
-    OUTPUT_FOLDER,
-    FLAIR_DETECT_BATCH_SIZE,
-)
 
 logger = get_logger()
 
@@ -40,78 +29,36 @@ async def flair_detect(
     output_blob_path: str,
     token: Annotated[HTTPAuthorizationCredentials, Depends(verify_token)],
 ):
-    # Set identifier for the current prediction (avoid overlap with async calls
-    prediction_id = str(uuid4())
+    try:
+        result = flair_detect_service(
+            image_bucket_name=image_bucket_name,
+            image_blob_path=image_blob_path,
+            model=model,
+            output_bucket_name=output_bucket_name,
+            output_blob_path=output_blob_path,
+        )
+    except NotFound as e:
+        raise HTTPException(
+            status_code=404,
+            detail=f"{str(e)}",
+        ) from e
+    except Forbidden as e:
+        raise HTTPException(
+            status_code=403,
+            detail=f"{str(e)}",
+        ) from e
+    except CalledProcessError as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Error executing flair-detect script: {e.stderr.strip()}",
+        ) from e
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"{str(e)}",
+        ) from e
+    finally:
+        # Force garbage collection to avoid future exceptions
+        gc.collect()
 
-    # Create output folder for the prediction
-    output_prediction_folder = os.path.join(OUTPUT_FOLDER, prediction_id)
-    os.makedirs(output_prediction_folder, exist_ok=True)
-
-    # Google clood storage client
-    client = Client(project=FLAIR_GCP_PROJECT)
-
-    # Download requested model from netcarbon gcs
-    flair_model, model_weights_path = get_requested_model(
-        model=model, client=client
-    )
-
-    # Download file to process
-    start_time = perf_counter()
-    image_local_path = download_file_to_process(
-        image_bucket_name=image_bucket_name,
-        image_blob_path=image_blob_path,
-        client=client,
-    )
-    download_file_duration = int(round(perf_counter() - start_time))
-
-    logger.info("download file duration: %s seconds", download_file_duration)
-
-    # Setup flair-detect config
-    output_name = os.path.basename(output_blob_path)
-    prediction_config_path = setup_config_flair_detect(
-        input_image_path=image_local_path,
-        model_weights_path=model_weights_path,
-        output_image_name=output_name,
-        output_folder=output_prediction_folder,
-        batch_size=FLAIR_DETECT_BATCH_SIZE,
-    )
-    logger.info(
-        "Config setup for flair-detect for model %s and image %s",
-        flair_model.name,
-        image_blob_path,
-    )
-
-    # Run the prediction with flair-detect script
-    use_gpu = torch.cuda.is_available()
-    logger.info("cuda is available : %s", use_gpu)
-
-    start_time = perf_counter()
-    result = run_prediction(prediction_config_path=prediction_config_path)
-    run_prediction_duration = int(round(perf_counter() - start_time))
-
-    logger.info("run prediction duration: %s seconds", run_prediction_duration)
-
-    # Upload resulted tif to bucket
-    start_time = perf_counter()
-    upload_result_to_bucket(
-        output_prediction_folder=output_prediction_folder,
-        output_name=output_name,
-        output_bucket_name=output_bucket_name,
-        output_blob_path=output_blob_path,
-        client=client,
-    )
-    upload_result_duration = int(round(perf_counter() - start_time))
-
-    logger.info("upload result duration: %s seconds", upload_result_duration)
-
-    return {
-        "prediction_id": prediction_id,
-        "message": f"prediction tif is available at gs://{output_bucket_name}/{output_blob_path}",
-        "cuda_used": use_gpu,
-        "result_stdout": result.stdout,
-        "perf": {
-            "download_file_duration": download_file_duration,
-            "run_prediction_duration": run_prediction_duration,
-            "upload_result_duration": upload_result_duration,
-        },
-    }
+    return result
