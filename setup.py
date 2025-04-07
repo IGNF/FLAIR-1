e0b79aa7c4d8880c22e6ec93492f302b94832e8d
Anatol Garioud <49435550+agarioud@users.noreply.github.com>
2025-04-07 17:05:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         "segmentation-models-pytorch==0.3.3",
         "albumentations==1.3.1",
         "tensorboard==2.15.1",
-        "transformers>=4.41.2"
+        "transformers<=4.50.3"
     ],
     include_package_data=True,
     package_data={
